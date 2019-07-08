ad1466748006d5d618670b7b435ad032879e57c2
Nayna <nainavaghasiya9@gmail.com>
2019-07-08 18:36:39 -0400

    Initial Commit

---
+++ day7TabView&PlistExample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  day7TabView&PlistExample
+//
+//  Created by Nayna on 7/8/19.
+//  Copyright © 2019 Nayna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
